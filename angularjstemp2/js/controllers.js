85cef00b53717af337a34880e4994f47ce657565
raj <rajnikanth.bs@omkarlabs.com>
2015-07-10 20:15:04 +0530

    new7-10-2015

--- angularjstemp2/js/controllers.js
+++ angularjstemp2/js/controllers.js
@@ -6,6 +6,26 @@ var homeng = angular.module('homeng', ['ngCookies', 'ngStorage']);
 
 
 //include Common menu in angular js
+homeng.filter('cut', function () {
+    return function (value, wordwise, max, tail) {
+        if (!value) return '';
+
+        max = parseInt(max, 10);
+        if (!max) return value;
+        if (value.length <= max) return value;
+
+        value = value.substr(0, max);
+        if (wordwise) {
+            var lastspace = value.lastIndexOf(' ');
+            if (lastspace != -1) {
+                value = value.substr(0, lastspace);
+            }
+        }
+
+        return value + (tail || ' …');
+    };
+});
+
 
 
 homeng.config(['$routeProvider',
